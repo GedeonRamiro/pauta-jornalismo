5af997447d54ea1c75d2aa3635542bc3ec7f9f19
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2023-06-17 09:28:25 -0300

    fix: return erro authorization

--- src/decorator/user-id.decorator.ts
+++ src/decorator/user-id.decorator.ts
@@ -1,8 +1,16 @@
-import { ExecutionContext, createParamDecorator } from '@nestjs/common';
+import {
+  ExecutionContext,
+  createParamDecorator,
+  ForbiddenException,
+} from '@nestjs/common';
 import { authorizantionToLoginPayload } from 'src/utils/base-64-converter';
 
 export const UserId = createParamDecorator((_, ctx: ExecutionContext) => {
   const { authorization } = ctx.switchToHttp().getRequest().headers;
+
+  if (!authorization) {
+    throw new ForbiddenException('Não tem permisão ou token inválido!');
+  }
 
   const loginPayload = authorizantionToLoginPayload(authorization);
 
