99331c4871296d2df4421379be9d7c9fe26bd190
Gedeon <gedeon_ramiro@hotmail.com>
2025-11-19 15:16:45 -0300

    refactor: cors

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'https://pauta-jornalismo-front.vercel.app',
+    ],
     credentials: true,
   });
 
   await app.listen(8080);
 }
+
 bootstrap();
